883af30462a075097e4f751a73c01e2dd34191c8
Hooregi <augustozerpa@protonmail.com>
2021-12-08 20:10:20 -0500

    (i3blocks) modified media script so it doesn't call mpd

--- .config/i3blocks/scripts/media.sh
+++ .config/i3blocks/scripts/media.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 MUS=$(playerctl metadata --format "{{ artist }} - {{ title }}" 2> /dev/null)
-MPD=$(cat "$HOME/.config/ncmpcpp/current-status")
-MPD_SONG=$(cat "$HOME/.config/ncmpcpp/current-song")
+#MPD=$(cat "$HOME/.config/ncmpcpp/current-status")
+#MPD_SONG=$(cat "$HOME/.config/ncmpcpp/current-song")
 S1=$(echo "$MPD" | sed -n '1p')
 if [[ $MUS ]]; then
     echo "  $MUS"
