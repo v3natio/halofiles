64d668fb0bb2ea2d652876b6cb37d6888440a1ad
Hooregi <augustozerpa@protonmail.com>
2021-12-08 20:52:42 -0500

    (scripts) fixed media script so mpv doesn't display

--- .config/i3blocks/scripts/media.sh
+++ .config/i3blocks/scripts/media.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MUS=$(playerctl metadata --format "{{ artist }} - {{ title }}" 2> /dev/null)
+MUS=$(playerctl -p spotify metadata --format "{{ artist }} - {{ title }}" 2> /dev/null)
 #MPD=$(cat "$HOME/.config/ncmpcpp/current-status")
 #MPD_SONG=$(cat "$HOME/.config/ncmpcpp/current-song")
 S1=$(echo "$MPD" | sed -n '1p')
