08ebaf35ff81148d368d08ae72da3965676c6d24
Hooregi <augustozerpa@protonmail.com>
2021-12-13 11:53:04 -0500

    (i3blocks) fixed wifi script so it shows network quality

--- .config/i3blocks/scripts/wifi.sh
+++ .config/i3blocks/scripts/wifi.sh
@@ -8,18 +8,17 @@ WS_0=" 睊 "
 WS_N=" 睊 "
 
 SSID=$(iwgetid -r)
-QUALITY=$(iw dev wlp6s0 link | grep 'dBm$' | grep -Eoe '-[0-9]{2}' | awk '{print  ($1 > -50 ? 100 :($1 < -100 ? 0 : ($1+100)*2))}')
 SSIG=$(grep "$(iwgetid -m | awk '{ printf "%s", $1 }')" /proc/net/wireless | awk '{ printf "%i\n", int($3 * 100 / 70) }')
 
 SIG=$(echo "$SSIG" | rev | cut -c 2- | rev)
 
 get_bat() {
     case $SIG in
-        0 | 1)	echo "$WS_0$QUALITY% " ;;
-        2 | 3)	echo "$WS_1$QUALITY% " ;;
-        4 | 5) 	echo "$WS_2$QUALITY% " ;;
-        6 | 7) 	echo "$WS_3$QUALITY% " ;;
-        *)		echo "$WS_4$QUALITY% "
+        0 | 1)	echo "$WS_0$SSIG% " ;;
+        2 | 3)	echo "$WS_1$SSIG% " ;;
+        4 | 5) 	echo "$WS_2$SSIG% " ;;
+        6 | 7) 	echo "$WS_3$SSIG% " ;;
+        *)		echo "$WS_4$SSIG% "
     esac
 }
 
