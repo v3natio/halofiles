7a6966efe22fbe8364dd5045e684b4a8a0bea191
Hooregi <augustozerpa@protonmail.com>
2021-12-29 11:54:24 -0500

    (i3/i3blocks) icons now color properly

--- .config/i3blocks/scripts/cpu.sh
+++ .config/i3blocks/scripts/cpu.sh
@@ -7,11 +7,6 @@ case $BLOCK_BUTTON in
         ;;
 esac
 
-def_icon1=
-def_col=#4c566a
-icon1=${1:-$def_icon1}
-col=${3:-$def_col}
-
 cpu_perc=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{printf "%.2f%\n", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
 
-echo " <span color='$col'>$icon1</span> $cpu_perc "
+echo "  $cpu_perc "
--- .config/i3blocks/scripts/media.sh
+++ .config/i3blocks/scripts/media.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MUS=$(playerctl -p spotify metadata --format "{{ trunc(artist, 18)}} - {{ trunc(title, 15)}}" 2> /dev/null)
-MED=$(playerctl -p mpv metadata --format "{{ trunc(title, 33)}}" 2> /dev/null)
+MUS=$(playerctl -p ncspot metadata --format "{{ trunc(artist, 18)}} - {{ trunc(title, 15)}}" 2> /dev/null)
+MED=$(playerctl -p mpv metadata --format "{{trunc(title, 33)}}" 2> /dev/null)
 if [[ $MUS ]]; then
     echo "  $MUS"
 elif [[ $MED ]]; then
